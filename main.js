2b4f29898c4cb4872f73f69a0087e2ae7b90cfcc
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-16 21:17:40 +0300

    step-2

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ let chevy = {
     convertible: false,
     mileage: 1021
 };
+
+chevy.needsWashing = true
+console.log(chevy);
+
+if (chevy.year < 1965) {
+    classic = true
+}
+
+for (let i = 0; i < chevy.passengers; i++){
+    addPersonToCar()
+}
