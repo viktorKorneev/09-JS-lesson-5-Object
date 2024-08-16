9f3f21ca05cdc56e9c9776eb383fb6c2ded691d2
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-16 21:24:48 +0300

    step-3

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
-let chevy = {
-    make: "Chevy",
-    model: "Bel Air",
-    year: "1957",
-    color: "red",
-    passengers: "2",
-    convertible: false,
-    mileage: 1021
-};
+// let chevy = {
+//     make: "Chevy",
+//     model: "Bel Air",
+//     year: "1957",
+//     color: "red",
+//     passengers: "2",
+//     convertible: false,
+//     mileage: 1021
+// };
 
-chevy.needsWashing = true
-console.log(chevy);
+// chevy.needsWashing = true
+// console.log(chevy);
 
-if (chevy.year < 1965) {
-    classic = true
+// if (chevy.year < 1965) {
+//     classic = true
+// }
+
+// for (let i = 0; i < chevy.passengers; i++){
+//     addPersonToCar()
+// }
+
+
+let dog = {
+    name: "Fido",
+    weight: 20.2,
+    age: 4,
+    breed: "mixed",
+    activity: "fetch balls"
 }
 
-for (let i = 0; i < chevy.passengers; i++){
-    addPersonToCar()
+let bark;
+if(dog.weight > 20) {
+    bark = "WOOF WOOF"
+} else {
+    bark = "woof woof"
 }
+
+let speak = `${dog.name} says ${bark} when he wants to ${dog.activity}`
+console.log(speak);
