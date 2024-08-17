049a693977722b6736fea568910bf8b947362585
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-17 13:49:59 +0300

    step-10

--- main.js
+++ main.js
@@ -115,35 +115,52 @@
 // console.log(secret);
 
 
-function makeCar () {
-    let makes = ["Chevy", "GM", "Fiat", "Webville Motors", "Tucker" ];
-    let models = ["Cadillac", "500", "Bel-Air", "Taxi", "Torpedo"];
-    let years = [1955, 1957, 1948, 1954, 1961 ];
-    let colors = ["red", "blue", "tan", "yellow", "white"];
-    let convertible = [true, false];
+// function makeCar () {
+//     let makes = ["Chevy", "GM", "Fiat", "Webville Motors", "Tucker" ];
+//     let models = ["Cadillac", "500", "Bel-Air", "Taxi", "Torpedo"];
+//     let years = [1955, 1957, 1948, 1954, 1961 ];
+//     let colors = ["red", "blue", "tan", "yellow", "white"];
+//     let convertible = [true, false];
 
-    let rand1 = Math.floor(Math.random() * makes.length);
-    let rand2 = Math.floor(Math.random() * models.length);
-    let rand3 = Math.floor(Math.random() * years.length);
-    let rand4 = Math.floor(Math.random() * colors.length);
-    let rand5 = Math.floor(Math.random() * 5) + 1;
-    let rand6 = Math.floor(Math.random() * 2);
+//     let rand1 = Math.floor(Math.random() * makes.length);
+//     let rand2 = Math.floor(Math.random() * models.length);
+//     let rand3 = Math.floor(Math.random() * years.length);
+//     let rand4 = Math.floor(Math.random() * colors.length);
+//     let rand5 = Math.floor(Math.random() * 5) + 1;
+//     let rand6 = Math.floor(Math.random() * 2);
 
 
-let car = {
-    make: makes[rand1],
-    model: models[rand2],
-    years: years[rand3],
-    color: colors[rand4],
-    passengers: rand5,
-    convertible: convertible[rand6]
-};
-return car;
-}
-function displayCar(car) {
-    console.log(`Your new car is a ${car.years} ${car.make} ${car.model} ${car.passengers} ${car.convertible}`);
+// let car = {
+//     make: makes[rand1],
+//     model: models[rand2],
+//     years: years[rand3],
+//     color: colors[rand4],
+//     passengers: rand5,
+//     convertible: convertible[rand6],
+//     mileage: 0
+// };
+// return car;
+// }
+// function displayCar(car) {
+//     console.log(`Your new car is a ${car.years} ${car.make} ${car.model} ${car.passengers} ${car.convertible}`);
     
-}
+// }
 
-let carToSell = makeCar();
-displayCar(carToSell)
+// let carToSell = makeCar();
+// displayCar(carToSell)
+
+
+let fiat = {
+    make: "Fiat",
+    model: "Bel Air",
+    year: "1957",
+    color: "Medium Blue",
+    passengers: "2",
+    convertible: false,
+    mileage: 1021,
+    drive: function () {
+        console.log("Zoom zoom!");
+        
+    }
+}
+fiat.drive()
