293e750038122beb15d275b8c6eaa8c80d365ace
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-17 14:24:49 +0300

    step-11

--- main.js
+++ main.js
@@ -150,28 +150,59 @@
 // displayCar(carToSell)
 
 
-let fiat = {
-    make: "Fiat",
-    model: "Bel Air",
-    year: "1957",
-    color: "Medium Blue",
-    passengers: "2",
-    convertible: false,
-    mileage: 1021,
-    started: false,
-    start: function () {
-        this.started = true
-    },
-    stop: function () {
-        this.started = false
-    },
-    drive: function () {
-        if (this.started) {
-            alert("Zoom zoom!")
-        } else {
-            alert("You need to start the engine first.")
-        }
+// let fiat = {
+//     make: "Fiat",
+//     model: "Bel Air",
+//     year: "1957",
+//     color: "Medium Blue",
+//     passengers: "2",
+//     convertible: false,
+//     mileage: 1021,
+//     started: false,
+//     start: function () {
+//         this.started = true
+//     },
+//     stop: function () {
+//         this.started = false
+//     },
+//     drive: function () {
+//         if (this.started) { 
+//             alert("Zoom zoom!")
+//         } else {
+//             alert("You need to start the engine first.")
+//         }
         
+//     }
+// }
+// fiat.drive();
+// fiat.start();
+// fiat.drive();
+// fiat.stop();
+
+
+
+
+
+
+let song = {
+    name: "Walk This Way",
+    artist: "Run-D.M.C.",
+    minutes: 4,
+    seconds: 3,
+    genre: "80s",
+    playing: false,
+    play: function () {
+        if (!this.playing) {
+            this.playing = true
+            console.log(`Playing ${this.name} by ${this.artist}`);
+            
+        }
+    },
+    pause: function () {
+        if (this.playing) {
+            this.playing = false
+        }
     }
 }
-fiat.drive()
+song.play()
+song.pause()
